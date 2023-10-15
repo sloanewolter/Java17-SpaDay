9a71cfa54022ec248bcab485f52f4fbfedcbb8e8
Sloane Wolter <sloanewolter@gmail.com>
2023-10-15 13:36:52 -0500

    finished studio

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,34 @@
+package org.launchcode.controllers;
+
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/add")
+    public String displayAddUserForm() {
+        return "user/add";
+
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("user", user);
+        if (verify.equals(user.getPassword())) {
+            return "/user/index";
+        } else {
+            String error = "Sorry, your passwords didn't match. Please try again.";
+            model.addAttribute("error", error);
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+            return "/user/add";
+        }
+    }
+}
